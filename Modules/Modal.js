939095813adf2a5f95fda60d30efc51f9b7314f5
Marvel Dev <marvellousalika48@gmail.com>
2020-08-31 19:18:00 +0100

    -initial module commit

---
+++ Modules/Modal.js
@@ -0,0 +1,12 @@
+const modal = () => {
+  return `
+    <div class="modal-content">
+      <p>Background</p>
+      <button>DEFAULT</button>
+      <button id="dim-button">DIM</button>
+      <button id="light-out-button">Lights Outs</button>
+    </div>
+  `
+}
+
+export default modal;
---
+++ Modules/RightNav.js
@@ -0,0 +1,54 @@
+const RightSideNav = () => {
+  return `
+    <section class="right-side-nav">
+      <i class="fa fa-search"></i>
+      <input type="text" id="input"placeholder="Search Twitter">
+      <div id="trending">
+        <h4>Trends for you</h4>
+        <div class="current-trend">
+          <p>Trending in Nigeria</p>
+          <strong>Erica</strong>
+          <p>Trending with: Laycon, #BBNaijaLockdown</p>
+          <p>128k Tweets</p>
+        </div>
+        <div class="current-trend">
+          <p>Trending in Nigeria</p>
+          <strong>Erica</strong>
+          <p>Trending with: Laycon, #BBNaijaLockdown</p>
+          <p>128k Tweets</p>
+        </div>
+        <div class="current-trend">
+          <p>Trending in Nigeria</p>
+          <strong>Erica</strong>
+          <p>Trending with: Laycon, #BBNaijaLockdown</p>
+          <p>128k Tweets</p>
+        </div>
+        <div class="current-trend">
+          <p>Trending in Nigeria</p>
+          <strong>Erica</strong>
+          <p>Trending with: Laycon, #BBNaijaLockdown</p>
+          <p>128k Tweets</p>
+        </div>
+        <div class="current-trend">
+          <p>Trending in Nigeria</p>
+          <strong>Erica</strong>
+          <p>Trending with: Laycon, #BBNaijaLockdown</p>
+          <p>128k Tweets</p>
+        </div>
+        <div class="current-trend">
+          <p>Trending in Nigeria</p>
+          <strong>Erica</strong>
+          <p>Trending with: Laycon, #BBNaijaLockdown</p>
+          <p>128k Tweets</p>
+        </div>
+        <div class="current-trend">
+          <p>Trending in Nigeria</p>
+          <strong>Erica</strong>
+          <p>Trending with: Laycon, #BBNaijaLockdown</p>
+          <p>128k Tweets</p>
+        </div>
+      </div>
+    </section>
+  `
+}
+export default RightSideNav;
